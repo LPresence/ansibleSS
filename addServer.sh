b1210d960ccf8e095b4ca069b5a6c06b5be64cc0
LPresence <lukas1er@wanadoo.fr>
2017-08-21 11:25:47 +0200

    Update addServer.sh

--- addServer.sh
+++ addServer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+#script permettant l'ajout et la configuration rapide et simple de machines distantes pour ansible
 
-#set -x
-#trap read debug
-
+#veuillez changer cette clé par votre clé id_rsa.pub personelle
 sshKeys='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJE7y7Bir+0CJ/vKB1EngUgRUAOQ8cH8LRsZgbSJ3x0umlyQetJPHj6SM5AMXBEQGK0Z3inDSL8NUjHCL5IA9NwbGW6RIO9NJpDNJtVWln2svLgByZIxUjUCT4GB2qft+FEaX/nqLh+yYV4O1/eWUQj2soVLuRVlHbZ9tnX+c5VFb7Q5iRR55OxpOGMM9qoAJUfYFQ/Vp0Q4agfJF6xuwEg6cA5P1EXTNGdd+hz8h0sAXbsXpPaxVdIcNWgOrt1NwVEVSn0mTuXq4efwuCMP2fFd0gsw3Krc5uJMAy2d6QEAZmDl+NAIMkV0m43ip861eVfl7g5dncTNd1Ph7Vz53N root@ubuntu
 '
 ansibleHostsPathMac='/opt/local/etc/ansible/hosts'
@@ -28,7 +27,7 @@ done
 # config ssh & login for automatic connection via ssh key
 ssh root@$serverIp /bin/bash <<ENDSSH
     # add our ssh keys to root user's authorized_keys
-    cd /root
+    cd /ansible
     mkdir -p .ssh
     echo "$sshKeys" >> .ssh/authorized_keys
 
@@ -44,7 +43,7 @@ ENDSSH
 echo "
 Host $serverName
 HostName $serverIp
-User root" >> ~/.ssh/config
+User ansible" >> ~/.ssh/config
 
 # add new server into ansible (use php instead of sed for OS X compatibility)
 serverGroupPhp='['$serverGroup']'
